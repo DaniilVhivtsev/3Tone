6b339041517c66aaa859eedde39484ea7f2f4fcd
Evgeniy Kuznetsov <eugeny.kuz@gmail.com>
2021-05-13 19:08:43 +0500

    Hot fixes for courses and added footer as snippet

--- courses.php
+++ courses.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="Assets/css/style.css">
     <link rel="stylesheet" href="Assets/css/style_courses.css">
     <title>Courses</title>
 </head>
@@ -13,30 +14,7 @@
 
       <!-- ------------------------------------header------------------------------------ -->
 
-    <header class="header">
-        <div class="container">
-            <div class="header_inner">
-
-                <div class="left_header">
-                    <div class="header_logo">
-                        <a href="index.html"><img src="Assets/images/logo/logo_white.PNG" class="logoIndex" alt=""></a>
-                    </div>
-
-                    <a href="">
-                        <img src="Assets/images/courses_page/sig_in.svg" class="sig_in_logo" alt="">
-                    </a>
-
-                </div>
-
-                <nav class="nav">
-                    <a class = "nav_link" href="#">Курсы</a>
-                    <a class = "nav_link" href="#">Форум</a>
-                    <a class = "nav_link" href="#">Практики</a>
-                </nav>
-            </div>
-        </div>
-    </header>
-
+    <?php require("Assets/snippets/header.php");?>
 
     <!-- ------------------------------------welcome_block------------------------------------ -->
     
@@ -112,7 +90,7 @@
           <div class="courese_content">
             <div class="photo_course_content">
               <img src="Assets/images/courses_page/image_5.svg" class="photo_course" alt="">
-              <img src="Assets/images/courses_page/image_6.svg" class="photo_course"alt="">
+              <img src="Assets/images/courses_page/image_6.svg" class="photo_course" alt="">
             </div>
             <div >
               <a href="#" class="button_to_start">Начать</a>
@@ -197,7 +175,7 @@
           <div class="offer_blocks">
 
             <div class="offer_img_cube">
-              <img src="Assets/images/cube_black.svg" style="width: 30px;"alt="">
+              <img src="Assets/images/cube_black.svg" style="width: 30px;" alt="">
             </div>
 
             <div class="offer_first_text">
@@ -217,60 +195,8 @@
 
 
     <!-- ------------------------------------footer------------------------------------ -->
-  <footer class="footer">
-    <div class="footer_block">
-      <div class="footer_block_row">
-        <div class="footer_block_column">
-          <div class="caption">Курсы</div>
-          <div class="footer_text">Курс 3-d от GHype</div>
-        </div>
-        <div class="footer_block_column">
-          <div class="caption">О 3Tone</div>
-          <div class="footer_text">Инвесторы</div>
-          <div class="footer_text">Руководство</div>
-          <div class="footer_text">Контактная информация</div>
-          <div class="caption">Сервисы</div>
-          <div class="footer_text">Онлаин практикум</div>
-        </div>
-        <div class="footer_block_column">
-          <div class="caption">Учетная запись</div>
-          <div class="footer_text">Аккаунт 3Tone</div>
-          <div class="footer_text">Условия учетной записи</div>
-        </div>
-        <div class="footer_block_column">
-          <div class="caption">Помощь</div>
-          <div class="footer_text">Поддержка</div>
-          <div class="footer_text">Карта сайта</div>
-          <div class="footer_text">Авторы</div>
-        </div>
-      </div>
 
-      <hr>
-
-      <div class="footer_block_row" style="padding-bottom: 0;">
-        <div class="footer_block_column">
-          <div class="caption">Условия использования</div>
-        </div>
-        <div class="footer_block_column">
-          <div class="caption">Политика конфиденциальности</div>
-        </div>
-        <div class="footer_block_column">
-          <div class="caption">Юридическая информация</div>
-        </div>
-        <div class="footer_block_column">
-          <div class="caption">Использование cookies</div>
-        </div>
-      </div>
-
-      <hr>
-
-      <div class="avtor_signature">
-        <img src="/Users/daniil/Documents/GitHub/3Tone/Assets/images/C.svg" alt="">
-        <div style="margin-left: 10px;">3Tone</div>
-        <div>,   2021 г.</div>
-      </div>
-    </div>
-  </footer>
+    <?php require("Assets/snippets/footer.php"); ?>
     
 </body>
 </html>
--- index.php
+++ index.php
@@ -125,60 +125,7 @@
     </div>
   </div>
 
-  <footer class="footer">
-    <div class="footer_block">
-      <div class="footer_block_row">
-        <div class="footer_block_column">
-          <div class="caption">Курсы</div>
-          <div class="footer_text">Курс 3-d от GHype</div>
-        </div>
-        <div class="footer_block_column">
-          <div class="caption">О 3Tone</div>
-          <div class="footer_text">Инвесторы</div>
-          <div class="footer_text">Руководство</div>
-          <div class="footer_text">Контактная информация</div>
-          <div class="caption">Сервисы</div>
-          <div class="footer_text">Онлаин практикум</div>
-        </div>
-        <div class="footer_block_column">
-          <div class="caption">Учетная запись</div>
-          <div class="footer_text">Аккаунт 3Tone</div>
-          <div class="footer_text">Условия учетной записи</div>
-        </div>
-        <div class="footer_block_column">
-          <div class="caption">Помощь</div>
-          <div class="footer_text">Поддержка</div>
-          <div class="footer_text">Карта сайта</div>
-          <div class="footer_text">Авторы</div>
-        </div>
-      </div>
-
-      <hr>
-
-      <div class="footer_block_row" style="padding-bottom: 0;">
-        <div class="footer_block_column">
-          <div class="caption">Условия использования</div>
-        </div>
-        <div class="footer_block_column">
-          <div class="caption">Политика конфиденциальности</div>
-        </div>
-        <div class="footer_block_column">
-          <div class="caption">Юридическая информация</div>
-        </div>
-        <div class="footer_block_column">
-          <div class="caption">Использование cookies</div>
-        </div>
-      </div>
-
-      <hr>
-
-      <div class="avtor_signature">
-        <img src="Assets/images/C.svg" alt="">
-        <div style="margin-left: 10px;">3Tone</div>
-        <div>,   2021 г.</div>
-      </div>
-    </div>
-  </footer>
+  <?php require("Assets/snippets/footer.php"); ?>
 
   </body>
 </html>
